b759cee43125755e67ea69866d6dbdb4f8e29fb5
Akshay <akshaykumar@caplineservices.com>
2026-06-29 15:10:45 +0530

    update : add nestjs backend

---
+++ server/src/expense/dto/create-expense.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsString,
+  IsNumber,
+  IsDateString,
+  IsNotEmpty,
+  IsInt,
+} from 'class-validator';
+
+export class CreateExpenseDto {
+  @IsString()
+  @IsNotEmpty()
+  category!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @IsNumber()
+  amount!: number;
+
+  @IsDateString()
+  date!: string;
+
+  @IsInt()
+  user_id!: string;
+}
